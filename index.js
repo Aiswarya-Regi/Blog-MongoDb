afd4e13d73eae8b0af604d53fd7cbe471a6fe053
Aiswarya <aiswaryaregi69@gmail.com>
2023-03-28 20:45:04 +0530

    blog mongodb

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const path = require('path')
+const MongoClient = require('mongoose');
+
+const CONSTANTS = require('./config/constant');
+const bannerRouter = require('./routes/banner');
+const bollywoodRouter = require('./routes/bollywood');
+const fitnessRouter = require('./routes/fitness');
+const foodRouter = require('./routes/food');
+const hollywoodRouter = require('./routes/hollywood');
+const placesRouter = require('./routes/places');
+const technologyRouter = require('./routes/technology');
+
+
+const app = express();
+app.use(cors())
+
+
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'public')));
+app.use("/banner",bannerRouter);
+app.use("/bollywood",bollywoodRouter);
+app.use("/fitness",fitnessRouter);
+app.use("/food",foodRouter);
+app.use("/hollywood",hollywoodRouter);
+app.use("/Travel",placesRouter);
+app.use("/technology",technologyRouter);
+
+
+app.listen(CONSTANTS.PORT, ()=>{
+    MongoClient.connect("mongodb+srv://Aishwarya:Aishwarya@cluster0.hpzakdu.mongodb.net/Blog?retryWrites=true&w=majority").then(() =>{
+    }).catch(err => console.log(err));
+    console.log(`Server is listening at http://localhost:${CONSTANTS.PORT}`)
+})
